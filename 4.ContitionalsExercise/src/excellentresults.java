9125bd745d8e432f66446f54810f8e03a6fbccf9
Yordan Milanov <106768384+YordanMilanov@users.noreply.github.com>
2022-06-05 23:55:48 +0300

    Add files via upload

---
+++ 4.ContitionalsExercise/src/excellentresults.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class excellentresults {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int grade = Integer.parseInt(scanner.nextLine());
+        if (grade >= 5) {
+            System.out.println("Excellent!");};
+    }
+}
